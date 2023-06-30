0b1edd8ab27a44b3f6f34e742e6d84bceff1932c
Alex James <alexander.james.developer@gmail.com>
2023-06-30 11:05:39 +0100

    Setup jni

--- pkgs/java_http/lib/src/java_client.dart
+++ pkgs/java_http/lib/src/java_client.dart
@@ -4,19 +4,35 @@
 
 library;
 
+import 'dart:io';
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:jni/jni.dart';
+import 'package:path/path.dart';
+
+import 'third_party/java/net/URL.dart';
 
 class JavaClient extends BaseClient {
+  void _initJVM() {
+    if (!Platform.isAndroid) {
+      Jni.spawnIfNotExists(dylibDir: join('build', 'jni_libs'));
+    }
+  }
+
   @override
   Future<StreamedResponse> send(BaseRequest request) async {
+    _initJVM();
+
+    final javaUrl = URL.ctor3(request.url.toString().toJString());
+    final dartUrl = Uri.parse(javaUrl.toString1().toDartString());
+
     const result = 'Hello World!';
     final stream = Stream.value(latin1.encode(result));
 
     return StreamedResponse(stream, 200,
         contentLength: 12,
-        request: request,
+        request: Request(request.method, dartUrl),
         headers: {'content-type': 'text/plain'},
         reasonPhrase: 'OK');
   }
